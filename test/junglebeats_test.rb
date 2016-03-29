9255d822eb77db040b9c0fce23da7cb4b75b8dbd
Megan McMullin <mcmullin.megan@gmail.com>
2016-03-29 15:24:44 -0600

    junglebeats_test.rb

---
+++ test/junglebeats_test.rb
@@ -0,0 +1,21 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require "../lib/node"
+require "../lib/linked_list"
+
+class Junglebeats_test < Minitest::Test
+
+
+
+  def test_when_one_item_head_next_node_is_empty
+
+  end
+
+  def test_it_knows_how_many_items_are_in_list
+
+  end
+
+  
+
+end
