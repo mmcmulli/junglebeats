d570ee9bec547be842bba34222a86f66c4a5af58
Megan McMullin <mcmullin.megan@gmail.com>
2016-03-29 15:23:38 -0600

    adding in node test

---
+++ test/node_test.rb
@@ -0,0 +1,25 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require "../lib/node"
+
+class Node_test < Minitest::Test
+
+  def test_new_node_created
+    node = Node.new("plop")
+    assert_equal "plop", node.data
+  end
+
+  def test_if_only_one_node_next_node_eql_nil
+    node = Node.new("plop")
+    node.next_node
+    assert node.next_node
+  end
+
+  def test_it_knows_what_is_in_next_node
+    node = Node.new("plop")
+    node.next_node
+    refute node.next_node
+  end
+
+end
