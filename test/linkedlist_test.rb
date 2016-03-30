ed606d6af53db1d5687be3e32712d13d49eb1645
Megan McMullin <mcmullin.megan@gmail.com>
2016-03-29 23:34:15 -0600

    iteration one, can only add two things to list

--- test/linkedlist_test.rb
+++ test/linkedlist_test.rb
@@ -2,23 +2,53 @@ gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
 require "../lib/linked_list"
+require "../lib/node"
 
-class LinkedList_test < Minitest::Test
+class LinkedListTest < Minitest::Test
 
-  def test_new_list_created
+  def test_new_list_created_head_is_empty_by_default
     list = LinkedList.new
+    refute list.head
   end
 
-  def test_list_is_empty_by_default
-    list = LinkedList.new
-  end
 
-  def test_it_can_add_one_piece_of_data_to_list
+  def test_it_can_append_one_piece_of_data_to_list
     list = LinkedList.new
     assert_equal "doop", list.append("doop")
   end
 
-  def test_has_a_string_representation
+
+  def test_if_only_one_node_next_node_points_to_nil ### ????
+    skip
+    list = LinkedList.new
+    refute list.head.next_node
+  end
+
+  def test_it_counts_one_piece_of_data
+    list = LinkedList.new
+    assert_equal 1, list.count
+  end
+
+
+  def test_has_a_string_representation_of_list
+    list = LinkedList.new
+    assert_equal "doop", list.to_string
+  end
+
+  def test_it_can_append_more_than_one_piece_of_data
+    skip
+    list = LinkedList.new
+    list.head
+    list.append("doop")
+    list.head
+    list.head.next_node
+    list.append("deep")
+    list.head.next_node
+    assert_equal ###### putting count sting???
+end
+
+  def test_it_can_count_multiple_additions
+    list = LinkedList.new
 
   end
 
--- test/node_test.rb
+++ test/node_test.rb
@@ -5,21 +5,26 @@ require "../lib/node"
 
 class Node_test < Minitest::Test
 
-  def test_new_node_created
+  def test_new_node_created_without_data
+    node = Node.new
+    refute node.data, "this node should be able to create without data"
+  end
+
+  def test_new_node_created_with_data
     node = Node.new("plop")
     assert_equal "plop", node.data
   end
 
   def test_if_only_one_node_next_node_eql_nil
     node = Node.new("plop")
-    node.next_node
-    refute node.next_node #because next node set default to nil, use refute? or should i not set default?
+    refute node.next_node
   end
 
   def test_it_knows_what_is_in_next_node
     node = Node.new("plop")
-    node.next_node
     refute node.next_node
   end
+
+  
 
 end
